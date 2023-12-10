d661292c03595ab51867bafb6e348629d85a8595
calliope-net <141074597+calliope-net@users.noreply.github.com>
2023-12-10 14:55:14 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,35 @@
+function funkTimeout () {
+    Calli2bot.i2cRESET_OUTPUTS()
+    OLEDtext.writeText8x16(1, 0, 7, oled.oled_text("TIMEOUT"))
+    Calli2bot.setRgbLed3(0xff0000, true, true, true, true, true)
+}
+function i2cSchleife () {
+    Calli2bot.setLed1(calli2bot.eLed.poweron, true, true, 1)
+    alle2Sekunden()
+}
+function alle2Sekunden () {
+    if (input.runningTime() - laufzeit_ms > 2000) {
+        laufzeit_ms = input.runningTime()
+        OLEDtext.writeText8x16(13, 0, 7, "" + Math.round(laufzeit_ms / 1000) + " " + "s", oled.eAlign.rechts)
+        OLEDtext.writeText8x16(14, 0, 7, wattmeter.statuszeile(wattmeter.wattmeter_eADDR(wattmeter.eADDR.Watt_x45), wattmeter.eStatuszeile.v), oled.eAlign.rechts)
+        OLEDtext.writeText8x16(15, 0, 7, wattmeter.statuszeile(wattmeter.wattmeter_eADDR(wattmeter.eADDR.Watt_x45), wattmeter.eStatuszeile.mA), oled.eAlign.rechts)
+        if (Calli2bot.geti2cError() == -1010 && bit.between(input.runningTime(), 11000, 20000)) {
+            oled.comment("wenn CalliBot 0x22 beim Start nicht angeschaltet war")
+            control.reset()
+        }
+    }
+}
+let laufzeit_ms = 0
+let Calli2bot: calli2bot.Calli2bot = null
+let OLEDtext: oled.oledclass = null
+let funkzeit_ms = 0
+OLEDtext = oled.new_oledclass(oled.oled_eADDR_OLED(oled.eADDR_OLED.OLED_16x8_x3C), false, true)
+wattmeter.reset(wattmeter.wattmeter_eADDR(wattmeter.eADDR.Watt_x45))
+Calli2bot = calli2bot.beimStart(calli2bot.calli2bot_eADDR(calli2bot.eADDR.CB2_x22))
+OLEDtext.set_eeprom_8x8(oled.oled_eEEPROM_Startadresse(oled.eEEPROM_Startadresse.F000))
+OLEDtext.writeText8x16(0, 0, 7, "TYP " + Calli2bot.i2cReadFW_VERSION(calli2bot.eVersion.Typ))
+radio.setFrequencyBand(1)
+radio.setGroup(222)
 basic.forever(function () {
 	
 })
